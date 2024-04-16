7a3d2a97c300b5fad117326a0aa5179e49ec54b9
Santiconquest <161180110+Santiconquest@users.noreply.github.com>
2024-04-16 04:18:24 +0000

    La funcionalidad del ejercicio se puede ver a traves de la consola

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  function dominio() {
+    let result = "";
+    for (let pro of pronoun) {
+      //console.log(pro);
+      for (let ad of adj) {
+        //console.log(ad);
+        for (let nou of noun) {
+          console.log(`${pro}${ad}${nou}.com`);
+        }
+      }
+    }
+  }
+
+  dominio();
 };
